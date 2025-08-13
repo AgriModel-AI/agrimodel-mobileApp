27e56bfdffc9e1a4bcac5cd4653fe28c80ecc421
Mugisha Abdoul Latif <85370865+MugAbdoul@users.noreply.github.com>
2025-08-13 02:29:57 +0200

    update codes

--- redux/persistConfig.ts
+++ redux/persistConfig.ts
@@ -1,31 +1,39 @@
-// Step 1: Configure Redux Persist
 // store/persistConfig.ts
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
-import communitiesReducer from './slices/communitiesSlice';
-import exploreReducer from './slices/exploreSlice';
-import networkReducer from './slices/networkSlice';
-import notificationsReducer from './slices/notificationSlice';
-import postsReducer from './slices/postsSlice';
-import predictReducer from './slices/predictSlice';
-import userDetailsReducer from './slices/userDetailsSlice';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import communitiesReducer from "./slices/communitiesSlice";
+import exploreReducer from "./slices/exploreSlice";
+import networkReducer from "./slices/networkSlice";
+import notificationsReducer from "./slices/notificationSlice";
+import postsReducer from "./slices/postsSlice";
+import predictReducer from "./slices/predictSlice";
+import userDetailsReducer from "./slices/userDetailsSlice";
 
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  whitelist: ['userDetails'], // Only persist userDetails slice
-};
-
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   userDetails: userDetailsReducer,
   network: networkReducer,
   communities: communitiesReducer,
   posts: postsReducer,
   explore: exploreReducer,
   notifications: notificationsReducer,
-  predict: predictReducer
+  predict: predictReducer,
 });
+
+const rootReducer = (state: any, action: any) => {
+  if (action.type === "LOGOUT") {
+    // Clear persisted storage
+    AsyncStorage.clear();
+    state = undefined; // This resets redux state
+  }
+  return appReducer(state, action);
+};
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["userDetails"],
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -34,13 +42,11 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        // Ignore these action types as they might contain non-serializable values
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
       },
     }),
 });
 
 export const persistor = persistStore(store);
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
